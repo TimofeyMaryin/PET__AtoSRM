6db1e2be581e06545e3a99b63346ad06c44da111
Timfoey Maryin <nemotim4ic@gmail.com>
2023-02-20 19:23:21 +0500

    feat: add application text model

---
+++ app/src/main/java/com/example/atosrm/presentation/ui/elements/text/LargeText.kt
@@ -0,0 +1,26 @@
+package com.example.atosrm.presentation.ui.elements.text
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import com.example.atosrm.presentation.ui.font.notoSans
+import androidx.compose.ui.unit.sp
+
+
+@Composable fun LargeText(
+    value: String,
+    modifier: Modifier = Modifier,
+    fontWeight: FontWeight = FontWeight.Normal,
+    color: Color = MaterialTheme.colorScheme.background,
+) =
+    Text(
+        text = value,
+        fontWeight = fontWeight,
+        fontFamily = notoSans,
+        color = color,
+        modifier = modifier,
+        fontSize = 20.sp
+    )
---
+++ app/src/main/java/com/example/atosrm/presentation/ui/elements/text/SmallText.kt
@@ -0,0 +1,23 @@
+package com.example.atosrm.presentation.ui.elements.text
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp 
+
+@Composable fun SmallText(
+    value: String,
+    modifier: Modifier = Modifier,
+    fontWeight: FontWeight = FontWeight.Normal,
+    color: Color = MaterialTheme.colorScheme.background,
+) =
+    Text(
+        text = value,
+        modifier = modifier,
+        fontWeight = fontWeight,
+        color = color,
+        fontSize = 12.sp
+    )
